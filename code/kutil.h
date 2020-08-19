a3da58a8dbe9ab2233ee8347cc444a32ab6edba3
Kyle <ssjmetroid@gmail.com>
2020-08-19 12:16:50 -0700

    really stupid hack to export struct debug symbols
    - improve clarity of the `defer` functionality macros
    - add safety static assertions to guarantee the size of kutil primitive
    variable type definitions

--- code/kutil.h
+++ code/kutil.h
@@ -18,10 +18,24 @@ template <typename F>
 privDefer<F> defer_func(F f) {
 	return privDefer<F>(f);
 }
-#define DEFER_1(x, y) x##y
-#define DEFER_2(x, y) DEFER_1(x, y)
-#define DEFER_3(x)    DEFER_2(x, __COUNTER__)
-#define defer(code)   auto DEFER_3(_defer_) = defer_func([&](){code;})
+#define CONCAT_(x,y)    x##y
+#define CONCAT(x,y)     CONCAT_(x,y)
+#define DEFER_UNIQUE(x) CONCAT(x,__COUNTER__)
+#define defer(code)     auto DEFER_UNIQUE(_defer_) = defer_func([&](){code;})
+/* This is a really stupid hack to force the export of struct symbols when there 
+	is no declared instances of the struct in the project.  If I can figure out 
+	a way to force the linker to export struct symbols for structs that have no 
+	variables defined (outside of internal code or implicitly via reinterpret 
+	casting of memory), then this should be deleted probably.  Another option to 
+	achieve this would be to use `__declspec( dllexport )` on MSVC for example, 
+	but I'm hoping this method is fully portable.
+	Example usage:
+		struct TestStruct
+		{
+			// ... some data declared in here
+		} FORCE_SYMBOL_EXPORT;*/
+#define FORCE_SYMBOL_EXPORT \
+	CONCAT(*hack_force_symbol_export_DO_NOT_USE_,__COUNTER__)
 /* Custom assert support. platform implementation required! */
 #define kassert(expression) do {\
 	platformAssert(static_cast<bool>(expression));\
@@ -32,16 +46,16 @@ privDefer<F> defer_func(F f) {
              formattedString, ##__VA_ARGS__)
 /* Universal primitive definitions with reasonable keystroke counts. */
 #include <stdint.h>
-using u8  = uint8_t;
-using u16 = uint16_t;
-using u32 = uint32_t;
-using u64 = uint64_t;
-using i8  = int8_t;
-using i16 = int16_t;
-using i32 = int32_t;
-using i64 = int64_t;
-using f32 = float;
-using f64 = double;
+using u8  = uint8_t;  static_assert(sizeof(u8)  == 1);
+using u16 = uint16_t; static_assert(sizeof(u16) == 2);
+using u32 = uint32_t; static_assert(sizeof(u32) == 4);
+using u64 = uint64_t; static_assert(sizeof(u64) == 8);
+using i8  = int8_t;   static_assert(sizeof(i8)  == 1);
+using i16 = int16_t;  static_assert(sizeof(i16) == 2);
+using i32 = int32_t;  static_assert(sizeof(i32) == 4);
+using i64 = int64_t;  static_assert(sizeof(i64) == 8);
+using f32 = float;    static_assert(sizeof(f32) == 4);
+using f64 = double;   static_assert(sizeof(f64) == 8);
 /* @TODO: allow the game to define the SoundSample size in the build script? */
 using SoundSample = i16;
 namespace kutil
