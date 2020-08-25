a9e049901c7be5a8d7c39c5dfd393b60bc405d78
Kyle <ssjmetroid@gmail.com>
2020-08-25 14:07:09 -0700

    samples: keyboard input
    - fix incorrect vscode C++ includePath

---
+++ samples/002-keyboard-input/code/KmlGame.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "TemplateGameState.h"
+struct GameState
+{
+	KmlTemplateGameState templateGameState;
+	struct 
+	{
+		v2f32 position;
+		kQuaternion orientation = kmath::IDENTITY_QUATERNION;
+	} samplePlayer;
+};
+global_variable GameState* g_gs;
+struct VertexNoTexture
+{
+	v3f32 position;
+	Color4f32 color;
+};
+const global_variable KrbVertexAttributeOffsets 
+	VERTEX_NO_TEXTURE_ATTRIBS = 
+		{ .position_3f32 = offsetof(VertexNoTexture, position)
+		, .color_4f32    = offsetof(VertexNoTexture, color)
+		, .texCoord_2f32 = sizeof(VertexNoTexture) };
+#define DRAW_LINES(mesh, vertexAttribs) \
+	g_krb->drawLines(mesh, CARRAY_SIZE(mesh), sizeof(mesh[0]), vertexAttribs)
+#define DRAW_TRIS(mesh, vertexAttribs) \
+	g_krb->drawTris(mesh, CARRAY_SIZE(mesh), sizeof(mesh[0]), vertexAttribs)
