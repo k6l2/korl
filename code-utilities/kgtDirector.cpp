5ba657e2936ecb4d6c0ca09b3d978fea9bb76acf
Kyle <ssjmetroid@gmail.com>
2020-11-19 16:08:15 -0800

    WIP KgtDirector
    - fleshed out basic director step function
    - add body collider to director

--- code-utilities/kgtDirector.cpp
+++ code-utilities/kgtDirector.cpp
@@ -22,19 +22,31 @@ internal bool
 }
 internal void 
 	kgtDirectorConstruct(
-		KgtDirector* d, KgtAllocatorHandle hKal, KgtDirectorActorId maxActors, 
-		KgtDirectorActorId maxEmployQueueSize)
+		KgtDirector* d, KgtAllocatorHandle hKal, KgtDirectorActorId maxActors)
 {
-	/* ensure that the pool index of the actor handle will always fit in a 
-		16-bit number if incrememnted by 1 */
+	/* Ensure that the pool index of the actor handle will always fit in a 
+		16-bit number if incrememnted by 1.  We do this so that a cleared actor 
+		handle is always invalid. */
 	korlAssert(maxActors < 0xFFFF);
 	*d = {};
-	d->hKal            = hKal;
-	d->maxActors       = maxActors;
-	d->roles = reinterpret_cast<KgtDirector::ActorRole*>(
+	d->hKal      = hKal;
+	d->maxActors = maxActors;
+	d->roles = reinterpret_cast<KgtDirector::Role*>(
 		kgtAllocAlloc(hKal, maxActors*sizeof(*d->roles)));
 	d->actors = reinterpret_cast<KgtActor*>(
 		kgtAllocAlloc(hKal, maxActors*sizeof(*d->actors)));
+	/* construct the data structure used to calculate geometry collision */
+	{
+		const KgtBodyColliderMemoryRequirements bcMemReqs = 
+			{ .maxShapes             = maxActors
+			, .maxBodies             = 
+				kmath::safeTruncateU32(2*maxActors)
+			, .maxCollisionManifolds = 
+				kmath::safeTruncateU32(maxActors*maxActors) };
+		const size_t bcBytes = kgtBodyColliderRequiredBytes(bcMemReqs);
+		void*const bcMemoryStart = kgtAllocAlloc(hKal, bcBytes);
+		d->bodyCollider = kgtBodyColliderConstruct(bcMemoryStart, bcMemReqs);
+	}
 	for(size_t a = 0; a < maxActors; a++)
 	{
 		d->actors[a].type = KgtActor::Type::ENUM_COUNT;
@@ -44,16 +56,18 @@ internal void
 internal KgtActor* 
 	kgtDirectorHire(KgtDirector* d, KgtActor::Type actorType)
 {
-#if 1
+#if 0
 	/* instead of immediately creating a new actor in the role slot, we add the 
 		actor to an employment queue, and at the end of the next call to step we 
-		actually add the actor to the roll */
+		actually add the actor to the roll????
+		Or actually, maybe we DO in fact add the actor, but it is simply marked 
+		as QUEUED until the end of the next step function */
 	korlAssert(!"TODO"); return nullptr;
 #else
 	KgtDirectorActorId freeRoleId = 0;
 	for(; freeRoleId < d->maxActors; freeRoleId++)
 	{
-		if(d->roles[freeRoleId].occupied)
+		if(d->roles[freeRoleId].state != KgtDirector::Role::State::VACANT)
 			continue;
 		break;
 	}
@@ -62,9 +76,8 @@ internal KgtActor*
 		KLOG(WARNING, "Director is full!");
 		return nullptr;
 	}
-	korlAssert(!d->roles[freeRoleId].occupied);
-	d->roles[freeRoleId].occupied = true;
-	d->roles[freeRoleId].fired    = false;
+	korlAssert(d->roles[freeRoleId].state == KgtDirector::Role::State::VACANT);
+	d->roles[freeRoleId].state = KgtDirector::Role::State::HIRE_QUEUED;
 	d->roles[freeRoleId].salt++;
 	KgtActor*const result = &d->actors[freeRoleId];
 	korlAssert(result->type == KgtActor::Type::ENUM_COUNT);
@@ -83,24 +96,91 @@ internal void
 	u16 actorSalt;
 	if(!kgtDirectorActorHandleParse(hActor, &roleId, &actorSalt))
 	{
-		KLOG(WARNING, "Attempted to fire actor using invalid handle!");
+		KLOG(WARNING, "Attempted to fire actor using invalid handle.");
 		return;
 	}
 	korlAssert(roleId < d->maxActors);
-	if(   d->roles[roleId].salt != actorSalt 
-	   || d->roles[roleId].state == KgtDirector::ActorRole::State::VACANT)
+	if(d->roles[roleId].salt != actorSalt)
 	{
+		KLOG(WARNING, "Attempted to fire stale actor (salt=%i, handleSalt=%i).", 
+		     d->roles[roleId].salt, actorSalt);
 		return;
 	}
-	d->roles[roleId].state = KgtDirector::ActorRole::State::FIRED;
+	/* we can only fire HIRED or HIRED_QUEUED actors */
+	if(   d->roles[roleId].state == KgtDirector::Role::State::FIRED 
+	   || d->roles[roleId].state == KgtDirector::Role::State::VACANT)
+	{
+		KLOG(WARNING, "Attempted to fire vacant actor (roleId=%i).", roleId);
+		return;
+	}
+	d->roles[roleId].state = KgtDirector::Role::State::FIRED;
 }
 internal KgtActor* 
 	kgtDirectorGetActor(KgtDirector* d, KgtActorHandle* hActor)
 {
-	korlAssert(!"TODO");return nullptr;
+	KgtDirectorActorId roleId;
+	u16 actorSalt;
+	if(!kgtDirectorActorHandleParse(hActor, &roleId, &actorSalt))
+	{
+		KLOG(WARNING, "Attempted to get actor using invalid handle.");
+		return nullptr;
+	}
+	korlAssert(roleId < d->maxActors);
+	if(d->roles[roleId].salt != actorSalt)
+	{
+		KLOG(WARNING, "Attempted to get stale actor (salt=%i, handleSalt=%i).", 
+		     d->roles[roleId].salt, actorSalt);
+		return nullptr;
+	}
+	KgtActor*const result = &d->actors[roleId];
+	return result;
 }
 internal void 
-	kgtDirectorStep(KgtDirector* d, f32 frameSeconds)
+	kgtDirectorStep(
+		KgtDirector* d, f32 frameSeconds, KgtAllocatorHandle hKalTemp)
 {
-	korlAssert(!"TODO");
+	/* update collision detection system */
+	kgtBodyColliderUpdateManifolds(d->bodyCollider, hKalTemp);
+	/* step HIRED actors */
+	/* @speed: use a temporary allocator to cache a list of pointers to hired 
+		actors */
+	/* @speed: maintain a hired actor count & stop iterating once we reached 
+		that # */
+	for(KgtDirectorActorId a = 0; a < d->maxActors; a++)
+	{
+		if(d->roles[a].state != KgtDirector::Role::State::HIRED)
+			continue;
+		kgtActorStep(&d->actors[a], d);
+	}
+	/* draw HIRED actors */
+	/* @speed: use the cached list of hired actors retrieved from the previous 
+		loop to waste less iteration here */
+	/* @speed: use a temporary allocator to cache a list of pointers to fired & 
+		hire_queued actors */
+	for(KgtDirectorActorId a = 0; a < d->maxActors; a++)
+	{
+		if(d->roles[a].state != KgtDirector::Role::State::HIRED)
+			continue;
+		kgtActorDraw(&d->actors[a], d);
+	}
+	/* nullify fired actors */
+	/* @speed: use the cached list of fired actors retrieved from the draw loop 
+		to waste less iteration here */
+	for(KgtDirectorActorId a = 0; a < d->maxActors; a++)
+	{
+		if(d->roles[a].state != KgtDirector::Role::State::FIRED)
+			continue;
+		kgtActorOnDestroy(&d->actors[a], d);
+		d->roles[a].state = KgtDirector::Role::State::VACANT;
+		d->actors[a].type = KgtActor::Type::ENUM_COUNT;
+	}
+	/* confirm hire queued actors */
+	/* @speed: use the cached list of hire_queued actors retrieved from the draw 
+		loop to waste less iteration here */
+	for(KgtDirectorActorId a = 0; a < d->maxActors; a++)
+	{
+		if(d->roles[a].state != KgtDirector::Role::State::HIRE_QUEUED)
+			continue;
+		d->roles[a].state = KgtDirector::Role::State::HIRED;
+	}
 }
--- code-utilities/kgtDirector.h
+++ code-utilities/kgtDirector.h
@@ -2,29 +2,35 @@
 #include "kutil.h"
 #include "kgtAllocator.h"
 #include "kgtActor.h"
+#include "kgtBodyCollider.h"
 using KgtDirectorActorId = u16;
 struct KgtDirector
 {
 	KgtAllocatorHandle hKal;// needed for destruction memory cleanup
 	KgtDirectorActorId maxActors;
-	struct ActorRole
+	struct Role
 	{
 		u16 salt;
+		/* only [HIRED | FIRED] actors will perform work during step */
 		enum class State : u8
 			{ VACANT
+			/* queued actors will NOT perform work, and become hired at the end 
+				of the current/next call to step */
 			, HIRE_QUEUED
+			/* hired actors will always perform work during step */
 			, HIRED
+			/* fired actors will STILL perform work for the duration of the next 
+				call to step, and will subsequently be nullified */
 			, FIRED } state;
 	} *roles;
 	KgtActor* actors;
+	KgtBodyCollider* bodyCollider;
 	f32 frameSeconds;// updated during director step
 };
 internal void 
 	kgtDirectorConstruct(
-		KgtDirector* d, KgtAllocatorHandle hKal, KgtDirectorActorId maxActors, 
-		KgtDirectorActorId maxEmployQueueSize);
+		KgtDirector* d, KgtAllocatorHandle hKal, KgtDirectorActorId maxActors);
 /**
- * 
  * @return A pointer to the actor data.
  */
 internal KgtActor* 
@@ -38,4 +44,5 @@ internal void
 internal KgtActor* 
 	kgtDirectorGetActor(KgtDirector* d, KgtActorHandle* hActor);
 internal void 
-	kgtDirectorStep(KgtDirector* d, f32 frameSeconds);
+	kgtDirectorStep(
+		KgtDirector* d, f32 frameSeconds, KgtAllocatorHandle hKalTemp);
