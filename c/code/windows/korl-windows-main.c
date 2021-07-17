b0d076020d31a267b1be33146d2d9b20d3a317ff
Kyle <ssjmetroid@gmail.com>
2021-07-17 10:27:03 -0700

    WIP C; allocate vulkan

--- c/code/windows/korl-windows-main.c
+++ c/code/windows/korl-windows-main.c
@@ -4,25 +4,43 @@
 #include "korl-memory.h"
 #include "korl-windows-window.h"
 #include <vulkan/vulkan.h>
+#include <vulkan/vulkan_win32.h>
 #include "korl-assert.h"
 /** MSVC program entry point must use the __stdcall calling convension. */
 void __stdcall korl_windows_main(void)
 {
     korl_log(INFO, "start");
     korl_memory_initialize();
-    /* test vulkan */
+    /* allocate vulkan */
+    KORL_ZERO_STACK(VkInstance, instance);
     {
-        KORL_ZERO_STACK(uint32_t, vkExtensionCount);
+        const char* enabledExtensions[] = 
+            { VK_KHR_SURFACE_EXTENSION_NAME
+            , VK_KHR_WIN32_SURFACE_EXTENSION_NAME };
+        KORL_ZERO_STACK(VkApplicationInfo, applicationInfo);
+        applicationInfo.sType            = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        applicationInfo.pApplicationName = "KORL Application";
+        applicationInfo.pEngineName      = "KORL";
+        applicationInfo.engineVersion    = 0;
+        applicationInfo.apiVersion       = VK_API_VERSION_1_2;
+        KORL_ZERO_STACK(VkInstanceCreateInfo, instanceCreateInfo);
+        instanceCreateInfo.sType                   = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        instanceCreateInfo.pApplicationInfo        = &applicationInfo;
+        instanceCreateInfo.enabledExtensionCount   = korl_arraySize(enabledExtensions);
+        instanceCreateInfo.ppEnabledExtensionNames = enabledExtensions;
         VkResult vkResult = 
-            vkEnumerateInstanceExtensionProperties(
-                NULL, &vkExtensionCount, NULL);
+            vkCreateInstance(
+                &instanceCreateInfo, NULL/*pAllocator*/, &instance);
         korl_assert(vkResult == VK_SUCCESS);
-        korl_log(INFO, "vkExtensionCount=%u", vkExtensionCount);
     }
     korl_windows_window_initialize(); 
     korl_windows_window_create(1024, 576);
     korl_windows_window_loop();
     korl_log(INFO, "end");
+    /* nullify vulkan */
+    {
+        vkDestroyInstance(instance, NULL/*pAllocator*/);
+    }
     ExitProcess(KORL_EXIT_SUCCESS);
 }
 #include "korl-assert.c"
