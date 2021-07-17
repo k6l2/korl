b00acb386039ee6fd277f98a2799b830d85a8c7d
Kyle <ssjmetroid@gmail.com>
2021-07-17 16:34:16 -0700

    WIP C; choose vulkan physical device

--- c/code/korl-vulkan.c
+++ c/code/korl-vulkan.c
@@ -1,17 +1,20 @@
 #include "korl-io.h"
 #include "korl-assert.h"
 #include <vulkan/vulkan.h>
+#if defined(_WIN32)
 #include <vulkan/vulkan_win32.h>
-typedef struct Korl_Vulkan_Context Korl_Vulkan_Context;
-struct Korl_Vulkan_Context
+#endif// defined(_WIN32)
+typedef struct _Korl_Vulkan_Context _Korl_Vulkan_Context;
+struct _Korl_Vulkan_Context
 {
     VkInstance instance;
+    VkPhysicalDevice physicalDevice;
 #if KORL_DEBUG
     PFN_vkDestroyDebugUtilsMessengerEXT vkDestroyDebugUtilsMessengerEXT;
     VkDebugUtilsMessengerEXT debugMessenger;
 #endif// KORL_DEBUG
 };
-korl_global_variable Korl_Vulkan_Context g_korl_vulkan_context;
+korl_global_variable _Korl_Vulkan_Context g_korl_vulkan_context;
 #if KORL_DEBUG
 korl_internal VkBool32 VKAPI_CALL _korl_vulkan_debugUtilsMessengerCallback(
     VkDebugUtilsMessageSeverityFlagBitsEXT      messageSeverity,
@@ -38,10 +41,47 @@ korl_internal VkBool32 VKAPI_CALL _korl_vulkan_debugUtilsMessengerCallback(
     return VK_FALSE;
 }
 #endif// KORL_DEBUG
+/** 
+ * \return true if \c deviceNew is a "better" device than \c deviceOld
+ */
+korl_internal bool _korl_vulkan_isBetterDevice(
+    const VkPhysicalDevice deviceOld, 
+    const VkPhysicalDevice deviceNew, 
+    const VkPhysicalDeviceProperties*const propertiesOld, 
+    const VkPhysicalDeviceProperties*const propertiesNew/*, 
+    const VkPhysicalDeviceFeatures*const featuresOld, 
+    const VkPhysicalDeviceFeatures*const featuresNew*/)
+{
+    korl_assert(deviceNew != VK_NULL_HANDLE);
+    KORL_ZERO_STACK(u32, queueFamilyCount);
+    vkGetPhysicalDeviceQueueFamilyProperties(
+        deviceNew, &queueFamilyCount, 
+        NULL/*pQueueFamilyProperties; NULL->return the count via param 2*/);
+    VkQueueFamilyProperties queueFamilies[64];
+    korl_assert(queueFamilyCount <= korl_arraySize(queueFamilies));
+    vkGetPhysicalDeviceQueueFamilyProperties(
+        deviceNew, &queueFamilyCount, queueFamilies);
+    bool hasGraphicsQueue = false;
+    for(u32 q = 0; q < queueFamilyCount; q++)
+        if(queueFamilies[q].queueFlags & VK_QUEUE_GRAPHICS_BIT)
+            hasGraphicsQueue = true;
+    /* don't even consider using devices that can't render graphics */
+    if(!hasGraphicsQueue)
+        return false;
+    /* if the old device hasn't been selected, just use the new device */
+    if(deviceOld == VK_NULL_HANDLE)
+        return true;
+    /* always attempt to use a discrete GPU first */
+    if(    propertiesNew->deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU
+        && propertiesOld->deviceType != VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU)
+        return true;
+    return false;
+}
 korl_internal void korl_vulkan_construct(void)
 {
-    Korl_Vulkan_Context*const context = &g_korl_vulkan_context;
+    _Korl_Vulkan_Context*const context = &g_korl_vulkan_context;
     VkResult vkResult = VK_SUCCESS;
+    /* nullify the context memory before doing anything for safety */
     memset(context, 0, sizeof(*context));
     /* get a list of VkLayerProperties so we can check of validation layer 
         support if needed */
@@ -105,7 +145,9 @@ korl_internal void korl_vulkan_construct(void)
         the platform */
     const char* enabledExtensions[] = 
         { VK_KHR_SURFACE_EXTENSION_NAME
+#if defined(_WIN32)
         , VK_KHR_WIN32_SURFACE_EXTENSION_NAME
+#endif// defined(_WIN32)
 #if KORL_DEBUG
         , VK_EXT_DEBUG_UTILS_EXTENSION_NAME
 #endif// KORL_DEBUG
@@ -155,14 +197,54 @@ korl_internal void korl_vulkan_construct(void)
         vkCreateDebugUtilsMessengerEXT(
             context->instance, &createInfoDebugUtilsMessenger, 
             NULL/*pAllocator*/, &context->debugMessenger);
+    korl_assert(vkResult == VK_SUCCESS);
 #endif// KORL_DEBUG
+    /* enumerate & choose a physical device */
+    KORL_ZERO_STACK(u32, physicalDeviceCount);
+    vkResult = 
+        vkEnumeratePhysicalDevices(
+            context->instance, &physicalDeviceCount, 
+            NULL/*pPhysicalDevices; NULL->return the count in param 2*/);
+    korl_assert(vkResult == VK_SUCCESS);
+    // we need at least one physical device compatible with Vulkan
+    korl_assert(physicalDeviceCount > 0);
+    VkPhysicalDevice physicalDevices[16];
+    korl_assert(physicalDeviceCount <= korl_arraySize(physicalDevices));
+    physicalDeviceCount = korl_arraySize(physicalDevices);
+    vkResult = 
+        vkEnumeratePhysicalDevices(
+            context->instance, &physicalDeviceCount, physicalDevices);
+    korl_assert(vkResult == VK_SUCCESS);
+    KORL_ZERO_STACK(VkPhysicalDeviceProperties, devicePropertiesBest);
+    KORL_ZERO_STACK(VkPhysicalDeviceFeatures, deviceFeaturesBest);
+    for(u32 d = 0; d < physicalDeviceCount; d++)
+    {
+        KORL_ZERO_STACK(VkPhysicalDeviceProperties, deviceProperties);
+        KORL_ZERO_STACK(VkPhysicalDeviceFeatures, deviceFeatures);
+        vkGetPhysicalDeviceProperties(physicalDevices[d], &deviceProperties);
+        vkGetPhysicalDeviceFeatures(physicalDevices[d], &deviceFeatures);
+        if(_korl_vulkan_isBetterDevice(
+                context->physicalDevice, physicalDevices[d], 
+                &devicePropertiesBest, &deviceProperties/*, 
+                &deviceFeaturesBest, &deviceFeatures*/))
+        {
+            context->physicalDevice = physicalDevices[d];
+            devicePropertiesBest = deviceProperties;
+            deviceFeaturesBest = deviceFeatures;
+        }
+    }
+    korl_assert(context->physicalDevice != VK_NULL_HANDLE);
+    korl_log(INFO, "chosen physical device: \"%s\"", 
+        devicePropertiesBest.deviceName);
 }
 korl_internal void korl_vulkan_destroy(void)
 {
-    Korl_Vulkan_Context*const context = &g_korl_vulkan_context;
+    _Korl_Vulkan_Context*const context = &g_korl_vulkan_context;
 #if KORL_DEBUG
     context->vkDestroyDebugUtilsMessengerEXT(
         context->instance, context->debugMessenger, NULL/*pAllocator*/);
 #endif// KORL_DEBUG
     vkDestroyInstance(context->instance, NULL/*pAllocator*/);
+    /* nullify the context after cleaning up properly for safety */
+    memset(context, 0, sizeof(*context));
 }
