7153df48c97b61af27e3c79a695550e299cee0e1
Kyle <ssjmetroid@gmail.com>
2023-04-01 20:25:44 -0700

    add gltf sample

---
+++ samples/gltf/code/game.cpp
@@ -0,0 +1,71 @@
+#include <stdlib.h>// needed for __FILEW__, etc...
+#include "korl-interface-game.h"
+#define _KORL_PLATFORM_API_MACRO_OPERATION(x) fnSig_##x *x;
+    #include "korl-interface-platform-api.h"
+    //KORL-ISSUE-000-000-120: interface-platform: remove KORL_DEFINED_INTERFACE_PLATFORM_API; this it just messy imo; if there is clear separation of code that should only exist in the platform layer, then we should probably just physically separate it out into separate source file(s)
+    #define KORL_DEFINED_INTERFACE_PLATFORM_API// prevent certain KORL modules which define symbols that are required by the game module, but whose codes are confined to the platform layer, from re-defining them since we just declared the API
+#undef _KORL_PLATFORM_API_MACRO_OPERATION
+korl_internal void _game_getInterfacePlatformApi(KorlPlatformApi korlApi)
+{
+    #define _KORL_PLATFORM_API_MACRO_OPERATION(x) (x) = korlApi.x;
+    #include "korl-interface-platform-api.h"
+    #undef _KORL_PLATFORM_API_MACRO_OPERATION
+}
+#include "korl-string.h"
+#include "korl-stringPool.h"
+#include "korl-logConsole.h"
+typedef struct Memory
+{
+    Korl_Memory_AllocatorHandle allocatorHeap;
+    Korl_Memory_AllocatorHandle allocatorStack;
+    bool continueRunning;
+    Korl_StringPool stringPool;
+    Korl_LogConsole logConsole;
+} Memory;
+korl_global_variable Memory* memory;
+KORL_EXPORT KORL_GAME_INITIALIZE(korl_game_initialize)
+{
+    _game_getInterfacePlatformApi(korlApi);
+    KORL_ZERO_STACK(Korl_Heap_CreateInfo, heapCreateInfo);
+    heapCreateInfo.initialHeapBytes = korl_math_megabytes(8);
+    const Korl_Memory_AllocatorHandle allocatorHeap = korl_memory_allocator_create(KORL_MEMORY_ALLOCATOR_TYPE_LINEAR, L"game", KORL_MEMORY_ALLOCATOR_FLAG_SERIALIZE_SAVE_STATE, &heapCreateInfo);
+    memory = KORL_C_CAST(Memory*, korl_allocate(allocatorHeap, sizeof(Memory)));
+    memory->allocatorHeap   = allocatorHeap;
+    memory->allocatorStack  = korl_memory_allocator_create(KORL_MEMORY_ALLOCATOR_TYPE_LINEAR, L"game-stack", KORL_MEMORY_ALLOCATOR_FLAG_EMPTY_EVERY_FRAME, &heapCreateInfo);
+    memory->continueRunning = true;
+    memory->stringPool      = korl_stringPool_create(allocatorHeap);
+    memory->logConsole      = korl_logConsole_create(&memory->stringPool);
+    korl_gui_setFontAsset(L"data/source-sans/SourceSans3-Semibold.otf");// KORL-ISSUE-000-000-086: gfx: default font path doesn't work, since this subdirectly is unlikely in the game project
+    return memory;
+}
+KORL_EXPORT KORL_GAME_ON_RELOAD(korl_game_onReload)
+{
+    _game_getInterfacePlatformApi(korlApi);
+    memory = KORL_C_CAST(Memory*, context);
+}
+KORL_EXPORT KORL_GAME_ON_KEYBOARD_EVENT(korl_game_onKeyboardEvent)
+{
+    if(isDown && !isRepeat)
+        switch(keyCode)
+        {
+        case KORL_KEY_ESCAPE:{
+            memory->continueRunning = false;
+            break;}
+        case KORL_KEY_GRAVE:{
+            korl_logConsole_toggle(&memory->logConsole);
+            break;}
+        default: break;
+        }
+}
+KORL_EXPORT KORL_GAME_UPDATE(korl_game_update)
+{
+    korl_logConsole_update(&memory->logConsole, deltaSeconds, korl_log_getBuffer, {windowSizeX, windowSizeY}, memory->allocatorStack);
+    return memory->continueRunning;
+}
+#include "korl-math.c"
+#include "korl-checkCast.c"
+#include "korl-string.c"
+#include "korl-stringPool.c"
+#include "korl-logConsole.c"
+#define STBDS_UNIT_TESTS // for the sake of detecting any other C++ warnings; we aren't going to actually run any of these tests
+#include "korl-stb-ds.c"
