b18e8a641b5582ce5a8374f365ab9df0347048b3
Kyle <ssjmetroid@gmail.com>
2024-02-12 10:28:13 -0800

    make the user provide KORL_PROJECT* vars
    this allows more flexibility when it comes to selecting which folders to
    put KORL scripts in, at very little cost

--- samples/gui/scripts/prompt.bat
+++ samples/gui/scripts/prompt.bat
@@ -1,17 +1,21 @@
 @echo off
-set KORL_EXE_NAME=korl-sample-gui
+set  KORL_EXE_NAME=korl-sample-gui
 echo KORL_EXE_NAME=%KORL_EXE_NAME%
-set KORL_EXE_VERSION=0
+set  KORL_EXE_VERSION=0
 echo KORL_EXE_VERSION=%KORL_EXE_VERSION%
-set KORL_GAME_TARGET_FRAMES_PER_SECOND=120
+set  KORL_GAME_TARGET_FRAMES_PER_SECOND=120
 echo KORL_GAME_TARGET_FRAMES_PER_SECOND=%KORL_GAME_TARGET_FRAMES_PER_SECOND%
 rem Maybe set this configuration in a more general "config" script at some point as an argument?
-set KORL_GAME_IS_DYNAMIC=TRUE
+set  KORL_GAME_IS_DYNAMIC=TRUE
 echo KORL_GAME_IS_DYNAMIC=%KORL_GAME_IS_DYNAMIC%
-cd %~dp0\..\..\..
+set  KORL_PROJECT_ROOT=%~dp0..\
+echo KORL_PROJECT_ROOT=%KORL_PROJECT_ROOT%
+set  KORL_PROJECT_SCRIPTS=%~dp0
+echo KORL_PROJECT_SCRIPTS=%KORL_PROJECT_SCRIPTS%
+cd %KORL_PROJECT_ROOT%\..\..\
 set "KORL_HOME=%cd%"
 echo KORL_HOME=%KORL_HOME%
 rem cmd /k option tells cmd to run the following command and then return to 
 rem the prompt, allowing us to continue using the cmd prompt window
-call "%windir%\system32\cmd.exe" /k "%KORL_HOME%\scripts\korl-set-build-environment.bat" %~dp0
+call "%windir%\system32\cmd.exe" /k "%KORL_HOME%\scripts\korl-set-build-environment.bat"
 
--- scripts/korl-set-build-environment.bat
+++ scripts/korl-set-build-environment.bat
@@ -1,5 +1,5 @@
 @echo off
-set oneFolderBelowProjectRoot=%1
+
 rem ----- setup MSVC build tools -----
 set vswhere_location=%programfiles(x86)%\Microsoft Visual Studio\Installer\vswhere
 for /f "usebackq delims=#" %%a in (`"%vswhere_location%" -latest -property installationPath`) do (
@@ -7,19 +7,22 @@ for /f "usebackq delims=#" %%a in (`"%vswhere_location%" -latest -property insta
 )
 echo "%VsDevCmd_Path%" -no_logo -arch=amd64 ...
 call "%VsDevCmd_Path%" -no_logo -arch=amd64
+
 rem ----- save VS environment variable defaults for later reconfiguration -----
 set "KORL_VS_ORIGINAL_INCLUDE=%INCLUDE%"
 set "KORL_VS_ORIGINAL_LIB=%LIB%"
 set "KORL_VS_ORIGINAL_LIBPATH=%LIBPATH%"
-rem ----- set the project root directory -----
-echo navigating to batch file's directory "%oneFolderBelowProjectRoot%"...
-cd %oneFolderBelowProjectRoot%
-cd ..
-set KORL_PROJECT_ROOT=%cd%
-echo KORL_PROJECT_ROOT=%KORL_PROJECT_ROOT%
+
+rem ----- move to the project's root directory -----
+rem       KORL build scripts will assume to start within this directory
+echo navigating to KORL_PROJECT_ROOT directory "%KORL_PROJECT_ROOT%"...
+cd %KORL_PROJECT_ROOT%
+
 rem ----- add KORL scripts to path for easy execution -----
-echo adding "%oneFolderBelowProjectRoot%" and "%KORL_HOME%\scripts" to path...
-set path=%KORL_HOME%\scripts;%oneFolderBelowProjectRoot%;%path%
+echo adding "%KORL_PROJECT_SCRIPTS%" and "%KORL_HOME%\scripts" to path...
+set path=%KORL_HOME%\scripts;%KORL_PROJECT_SCRIPTS%;%path%
+
 rem ----- initialize environment in DEBUG mode, since it will be most common -----
 call korl-build-configure-debug.bat
+
 echo KORL Build Environment Ready!
